59e4fb14cf3b6573cd16b2951640e54aa1437a2e
Valyn <115489989+ValynTyler@users.noreply.github.com>
2024-04-30 12:51:56 +0300

    Moved assets into separate repo

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,9 @@ use std::path::Path;
 use suoi_simp::obj::Obj;
 
 fn main() {
-    let path = Path::new("assets/models/cube/cube.obj");
+    let path = Path::new("../suoi_assets/models/cube.obj");
     assert_eq!(path.extension().unwrap(), "obj");
 
-    let mesh = Obj::import(path).expect("Error");
+    let mesh = Obj::import(path).expect("ImportError");
     println!("{:#?}", mesh);
 }
